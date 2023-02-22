7325972518a7abed1b6c348898bd1681866fca2b
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-02-22 17:36:06 -0300

    Gerando componente HOME

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -22,12 +22,13 @@ import { MatSnackBarModule } from "@angular/material/snack-bar";
 import { HeaderComponent } from './views/components/template/header/header.component';
 import { FooterComponent } from './views/components/template/footer/footer.component';
 import { NavComponent } from './views/components/template/nav/nav.component';
+import { HomeComponent } from './views/components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent, 
     HeaderComponent, 
-    FooterComponent, NavComponent
+    FooterComponent, NavComponent, HomeComponent
   ],
   imports: [
     BrowserModule,
