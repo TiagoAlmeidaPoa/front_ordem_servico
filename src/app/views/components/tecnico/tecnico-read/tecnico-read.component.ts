e2e2fe3fab8e7068ec289cacf89fd04400610b3b
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-02-25 14:14:08 -0300

    Realizando ajuste na paginacao Tecnico

--- src/app/views/components/tecnico/tecnico-read/tecnico-read.component.ts
+++ src/app/views/components/tecnico/tecnico-read/tecnico-read.component.ts
@@ -20,15 +20,15 @@ export class TecnicoReadComponent implements AfterViewInit {
 
   constructor(private service : TecnicoService) {}
 
-  ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+  ngAfterViewInit() {    
     this.findAll();
   }
 
   findAll():void {
     this.service.findAll().subscribe((resposta) => {
       this.tecnicos = resposta;
-      console.log(this.tecnicos);
+      this.dataSource = new MatTableDataSource<Tecnico>(this.tecnicos);
+      this.dataSource.paginator = this.paginator;
     })
   }
 
