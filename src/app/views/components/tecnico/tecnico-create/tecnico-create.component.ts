7f404f0a073777fed0e755356c0cbdf6f969a0ef
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-02-28 21:51:27 -0300

    validando campos do formulario de criacao de Tecnico

--- src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
+++ src/app/views/components/tecnico/tecnico-create/tecnico-create.component.ts
@@ -1,7 +1,10 @@
 import { Component } from "@angular/core";
+import { FormControl, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { Tecnico } from "src/app/models/tecnico";
 import { TecnicoService } from "src/app/services/tecnico.service";
+
+let msgCpfInvalido = "número do registro de contribuinte individual brasileiro (CPF) inválido"
 
 @Component({
   selector: "app-tecnico-create",
@@ -11,10 +14,14 @@ import { TecnicoService } from "src/app/services/tecnico.service";
 export class TecnicoCreateComponent {
   tecnico: Tecnico = {
     id: "",
-    nome: "Tiago Almeida",
-    cpf: "82574112059",
-    telefone: "(00) 90000-00",
+    nome: "",
+    cpf: "",
+    telefone: "",
   };
+
+  nome = new FormControl('', [Validators.minLength(5)])
+  cpf = new FormControl('', [Validators.minLength(11)])
+  telefone = new FormControl('', [Validators.minLength(11)])
 
   constructor(private router: Router, private service: TecnicoService) {}
 
@@ -31,8 +38,32 @@ export class TecnicoCreateComponent {
       (err) => {
         if (err.error.message.match("já cadastrado")) {
           this.service.message(err.error.message);
+        } else if(err.error.errors[0].message === msgCpfInvalido){
+          this.service.message("CPF inválido");
         }
       }
     );
   }
+
+  errorValidName() {
+    if(this.nome.invalid) {
+      return 'O nome deve ter entre 5 e 100 caracteres!';
+    }
+    return null;
+  }
+
+  errorValidCPF() {
+    if(this.cpf.invalid) {
+      return 'O cpf deve ter 11 caracteres!';
+    }
+    return null;
+  }
+
+  errorValidTelefone() {
+    if(this.telefone.invalid) {
+      return 'O telefone deve ter 11 caracteres!';
+    }
+    return null;
+  }
+
 }
