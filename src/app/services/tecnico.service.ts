51ad6409bf6366ff32e1fdbd7efe4692c7f093a6
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-03-02 20:03:50 -0300

    Criando metodo update para Tecnico

--- src/app/services/tecnico.service.ts
+++ src/app/services/tecnico.service.ts
@@ -22,9 +22,19 @@ export class TecnicoService {
     return this.http.get<Tecnico[]>(url);
   }
 
+  findById(id : any):Observable<Tecnico> {
+    const url = `${this.baseUrl}/tecnicos/${id}`
+    return this.http.get<Tecnico>(url);
+  }
+
   create(tecnico: Tecnico):Observable<Tecnico> {
     const url = this.baseUrl + "/tecnicos";
     return this.http.post<Tecnico>(url, tecnico);
+  }
+
+  update(tecnico: Tecnico):Observable<Tecnico> {
+    const url = `${this.baseUrl}/tecnicos/${tecnico.id}`;
+    return this.http.put<Tecnico>(url, tecnico);
   }
 
   message(msg : string) : void {
--- src/app/views/components/tecnico/tecnico-update/tecnico-update.component.ts
+++ src/app/views/components/tecnico/tecnico-update/tecnico-update.component.ts
@@ -1,8 +1,10 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { FormControl, Validators } from "@angular/forms";
-import { Router } from "@angular/router";
+import { ActivatedRoute, Router } from "@angular/router";
 import { Tecnico } from "src/app/models/tecnico";
 import { TecnicoService } from "src/app/services/tecnico.service";
+
+let msgCpfInvalido = "número do registro de contribuinte individual brasileiro (CPF) inválido"
 
 @Component({
   selector: "app-tecnico-update",
@@ -10,7 +12,9 @@ import { TecnicoService } from "src/app/services/tecnico.service";
   styleUrls: ["./tecnico-update.component.css"],
 })
 
-export class TecnicoUpdateComponent {
+export class TecnicoUpdateComponent implements OnInit {
+
+  id_tec = '';
 
   tecnico: Tecnico = {
     id: "",
@@ -23,7 +27,35 @@ export class TecnicoUpdateComponent {
   cpf = new FormControl('', [Validators.minLength(11)])
   telefone = new FormControl('', [Validators.minLength(11)])
 
-  constructor(private router: Router, private service: TecnicoService) {}
+  constructor(
+    private router: Router, 
+    private service: TecnicoService,
+    private route: ActivatedRoute
+    ) {}
+
+  ngOnInit(): void {
+    this.id_tec = this.route.snapshot.paramMap.get('id')!
+    this.findById();
+  }
+
+  update():void {
+    this.service.update(this.tecnico).subscribe(resposta => {
+      this.router.navigate(["/tecnicos"]);
+      this.service.message('Técnico atualizado com sucesso !');
+    },(err) => {
+      if (err.error.message.match("já cadastrado")) {
+        this.service.message(err.error.message);
+      } else if(err.error.errors[0].message === msgCpfInvalido){
+        this.service.message("CPF inválido");
+      }
+    })
+  }
+  
+  findById(): void {
+    this.service.findById(this.id_tec).subscribe(resposta => {
+      this.tecnico = resposta;
+    })
+  }
 
   cancel(): void {
     this.router.navigate(["/tecnicos"]);
