0a7f440ef6967dd734cf360dceaef314005d6b76
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-03-08 18:10:11 -0300

    implementando o servico para Ordem de Servico

---
+++ src/app/models/os.ts
@@ -0,0 +1,10 @@
+export interface OS {
+    id?: any;
+    dataAbertura?: any;
+    dataFechamento?: any;
+    prioridade: any;
+    obervacoes: String;
+    status: any;
+    tecnico: any;
+    cliente: any;
+}
