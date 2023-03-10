e517bb0fe5d26ee20eded2718f5617fd02e19dab
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-03-10 17:19:01 -0300

    Ajustando READ para Ordem de Servico (Tecnico e Cliente) traz nome em vez de id

--- src/app/views/components/os/os-read/os-read.component.ts
+++ src/app/views/components/os/os-read/os-read.component.ts
@@ -3,7 +3,9 @@ import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
 import { Router } from '@angular/router';
 import { OS } from 'src/app/models/os';
+import { ClienteService } from 'src/app/services/cliente.service';
 import { OsService } from 'src/app/services/os.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-os-read',
@@ -21,7 +23,10 @@ export class OsReadComponent implements AfterViewInit {
 
   constructor(
     private service : OsService,
-    private router:Router) {}
+    private router:Router,
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService
+    ) {}
 
   ngAfterViewInit() {    
     this.findAll();
@@ -30,6 +35,8 @@ export class OsReadComponent implements AfterViewInit {
   findAll():void {
     this.service.findAll().subscribe((resposta) => {
       this.ordensServico = resposta;
+      this.listarTecnico();
+      this.listarCliente();
       this.dataSource = new MatTableDataSource<OS>(this.ordensServico);
       this.dataSource.paginator = this.paginator;
     })
@@ -37,6 +44,22 @@ export class OsReadComponent implements AfterViewInit {
 
   navigateToCreate():void {
     this.router.navigate(['ordemservicos/create'])
+  }
+
+  listarTecnico():void {
+    this.ordensServico.forEach(os => {
+      this.tecnicoService.findById(os.tecnico).subscribe(resposta => {
+        os.tecnico = resposta.nome
+      })
+    })
+  }
+
+  listarCliente():void {
+    this.ordensServico.forEach(os => {
+      this.clienteService.findById(os.cliente).subscribe(resposta => {
+        os.cliente = resposta.nome
+      })
+    })
   }
 
 }
