165d22156f071b2e6dafc3436f54b8292fd8d47a
Tiago Almeida <tiago.almeida.poa@gmail.com>
2023-03-12 18:22:49 -0300

    Adicionando tela de criacao de ordem de servico usando angular material

--- src/app/views/components/os/os-create/os-create.component.ts
+++ src/app/views/components/os/os-create/os-create.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cliente } from 'src/app/models/cliente';
+import { Tecnico } from 'src/app/models/tecnico';
+import { ClienteService } from 'src/app/services/cliente.service';
+import { TecnicoService } from 'src/app/services/tecnico.service';
 
 @Component({
   selector: 'app-os-create',
   templateUrl: './os-create.component.html',
   styleUrls: ['./os-create.component.css']
 })
-export class OsCreateComponent {
+export class OsCreateComponent implements OnInit {
 
+  selected = ''
+
+  tecnicos: Tecnico[] = [];
+  clientes: Cliente[] = [];
+
+  constructor(
+    private tecnicoService: TecnicoService,
+    private clienteService: ClienteService
+  ) {}
+
+  ngOnInit(): void {
+    this.listarTecnicos();
+    this.listarClientes();
+  }
+
+  listarTecnicos(): void {
+    this.tecnicoService.findAll().subscribe(resposta => {
+      this.tecnicos = resposta;
+    })
+  }
+
+  listarClientes(): void {
+    this.clienteService.findAll().subscribe(resposta => {
+      this.clientes = resposta;
+    })
+  }
 }
